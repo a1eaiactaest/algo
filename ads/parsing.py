5070bc109a86a50acfc4e149e76b4567a333734a
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-07-05 10:32:53 +0200

    postfix calculator

--- ads/parsing.py
+++ ads/parsing.py
@@ -12,3 +12,26 @@ def valid_parentheses(line: str) ->  bool:
     return True
   return False
 
+def postfix(line: str) -> float:
+  ops = ['*', '+', '-', '/']
+  stack = []
+  if ' ' in line:
+    broken_line = line.split()
+  else:
+    broken_line = list(line)
+
+  for c in broken_line:
+    if c in ops:
+      b = stack.pop()
+      a = stack.pop()
+      if c == '*':
+        stack.append(a*b)
+      elif c == '+':
+        stack.append(a+b)
+      elif c == '-':
+        stack.append(a-b)
+      elif c == '/':
+        stack.append(a/b)
+    else:
+      stack.append(float(c))
+  return stack.pop()
--- test/test_parsing.py
+++ test/test_parsing.py
@@ -2,7 +2,29 @@
 
 import unittest
 
+from ads.parsing import postfix
 from ads.parsing import valid_parentheses
+
+class TestPosFix(unittest.TestCase):
+  def test_spaces(self):
+    equation_string = "3 5 * 4 +"
+    self.assertEqual(postfix(equation_string), 19.0) 
+    del equation_string
+
+  def test_no_spaces(self):
+    equation_string = "35*4+" # in case of joined numbers postif splits them. it's 3 and 5 not 35.
+    self.assertEqual(postfix(equation_string), 19.0) 
+    del equation_string
+
+  def test_complex_no_spaces(self):
+    equation_string = "23-4+567*+*"
+    self.assertEqual(postfix(equation_string), 141.0) 
+    del equation_string
+
+  def test_complex_spaces(self):
+    equation_string = "2 3 - 4 + 5 6 7 * + *"
+    self.assertEqual(postfix(equation_string), 141.0) 
+    del equation_string
 
 class TestVlidParetheses(unittest.TestCase):
   def test_round(self):
