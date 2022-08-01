88e3c0f10b3f35cd31a425e00f749f91e7c59ed4
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-08-01 13:22:26 +0200

    rewrite parentheses validation

--- ads/strings/parentheses.py
+++ ads/strings/parentheses.py
@@ -1,13 +1,15 @@
 def valid(line: str) ->  bool:
   stack = []
+  p = {')':'(', '}': '{', ']':'['}
+
   for c in line:
-    if c in ['(', '[', '{']:
-      stack.append(c)
-    else:
-      if stack == []:
-        return False
-      if c in [')', ']', '}']:
+    if c in p:
+      if stack and stack[-1] == p[c]:
         stack.pop()
-  if stack == []:
+      else:
+        return False
+    else:
+      stack.append(c)
+  if not stack:
     return True
   return False
