f637fd858519e4ff345ed98d0d253a69bf457bd8
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-08-05 13:59:44 +0200

    update strings/ints

--- ads/strings/ints.py
+++ ads/strings/ints.py
@@ -1,17 +1,17 @@
 import re
 
-def ints(string):
-  """Takes a string and returns all ints in it.
+def ints(string: str):
+  """Takes a string and returns all ints in it in the sorted order.
   
   _Example:
 
-    >>> ints(string)
-    [2, 1]
+    >>> ints("i have 1 cat and 2 dogs")
+    [1, 2]
     >>> ints("my favorite number is 1337, not one thousand three hundred thirty seven")
     [1337]
     >>> ints("he33llo 42 I'm a 32 string 30")
-    [33, 42, 30]
+    [30, 32, 33, 42]
 
   """
 
-  return [int(s) for s in re.findall(r'\b\d+\b', string)]
+  return sorted([int(s) for s in re.findall(r'\d+', string)])
