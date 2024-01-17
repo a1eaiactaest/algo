952f7e8f3665d906af50017041a8d13ad32c6ae0
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-01-17 16:44:39 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -4,25 +4,26 @@ import os
 from setuptools import setup
 
 
-directory = os.path.abspath(os.path.dirname(__file))
+directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
-setup(name='abs',
+setup(name='ads',
       version='0.1.0',
       description='Package of curated Algorithms and Data Structures written by the community',
       author='Piotr Rybiec',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages=['abs']
+      packages=['ads',],
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
 
       ],
-      python_requires>='3.9',
-      include_package_data=True)
+      include_package_data=True,
+      python_requires='>=3.9'
+)
 
 
 
