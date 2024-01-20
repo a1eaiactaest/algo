72e58efaab7df8bed176c7beaf89bf67492c30bc
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-01-20 13:11:57 +0100

    measure flops helper

--- ads/helpers.py
+++ ads/helpers.py
@@ -25,4 +25,25 @@ class Timing(contextlib.ContextDecorator):
     if self.enabled:
       print(f'{self.prefix}{self.t*1e-6:6.2} ms'+(self.on_exit(self.t) if self.on_exit else ''))
 
+class MeasureFLOPS(contextlib.ContextDecorator):
+  ups = {
+    'M': 1e-6,
+    'G': 1e-9,
+    'T': 1e-12,
+    'P': 1e-15,
+  }
+  def __init__(self, Nflop:int, unit_prefix='G', prefix='', on_exit=None, enabled=True):
+    self.Nflop = Nflop
+    self.unit_prefix = unit_prefix
+    self.prefix = prefix
+    self.on_exit = on_exit
+    self.enabled = enabled
+  def __enter__(self): self.st = time.monotonic()
+  def __exit__(self, *exc):
+    et = time.monotonic()
+    self.t = et - self.st
+    flops = self.Nflop/self.t
+    if self.enabled:
+      print(f'{self.prefix}{(flops*self.ups[self.unit_prefix]):.2f} {self.unit_prefix}FLOPS, {self.t*1e3:.2f} ms'+(self.on_exit(self.t) if self.on_exit else ''))
+        
 
