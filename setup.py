308d19220fbb0f2ccc5e40bf6ad7dd227c5f10db
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-04-17 19:34:42 +0200

    update setup and reqs

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
-setup(name='ads',
-      version='0.1.0',
-      description='Package of curated Algorithms and Data Structures written by the community',
+setup(name='algo',
+      version='0.6.9',
+      description='',
       author='Piotr Rybiec',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages=['ads',],
+      packages=['algo',],
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
